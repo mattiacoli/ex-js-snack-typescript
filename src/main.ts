bbd5dca9211c8b77ede876ba39c65a919a720765
mattiacoli <mattia.coli@aol.com>
2025-07-09 10:05:08 +0200

    Snack 3

--- src/main.ts
+++ src/main.ts
@@ -46,3 +46,27 @@ type Dipendente = {
   readonly emailAziendale: string
   contratto: "indeterminato" | "determinato" | "freelance"
 }
+
+/* Snack 3
+Estendiamo Dipendente per definire due ruoli specifici all'interno dell'azienda:
+    Developer
+        livelloEsperienza → Il livello di esperienza del developer (le scelte possibili sono solo “Junior”, “Mid” o “Senior”).
+        linguaggi → Un array contenente i linguaggi di programmazione utilizzati dal developer in azienda (opzionale, perché i neo assunti non hanno ancora dei linguaggi assegnati).
+        certificazioni → Un array di stringhe contenente certificazioni tecniche ottenute dal developer (può essere vuoto).
+    ProjectManager
+        teamSize → Il numero di persone nel team gestito dal Project Manager (può essere null se non ha ancora un team assegnato).
+        budgetGestito → Il totale del budget annuale gestito dal PM (opzionale).
+        stakeholderPrincipali → Un array di stringhe con i nomi dei principali stakeholder con cui il PM collabora (può essere vuoto).
+ */
+
+type Developer = Dipendente & {
+  livelloEsperinza: "Junior" | "Mid" | "Senior"
+  linguaggi?: string[]
+  certificazioni: string[]
+}
+
+type ProjectManager = Dipendente & {
+  teamSize: number | null
+  budgetGestito?: number
+  stakeholderPrincipali: string[]
+}
